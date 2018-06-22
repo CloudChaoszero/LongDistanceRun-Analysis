33af4fe5ec56405192e6859eab37e04a65d6784f
CloudChaoszero <rmaldonadocloud@outlook.com>
2018-06-22 13:43:02 -0700

    Intiate interaction of server receiving data and visualization

--- Analysis/app.py
+++ Analysis/app.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template,jsonify
+import os
+import sys
+sys.path.insert(0,os.path.join('..','Resources','AccessInformation'))
+from accessinformation import access_token
 
+from flask import Flask, render_template,jsonify
 from stravalib import Client , unithelper
 
-client = Client(access_toke )
+import json
+
+client = Client(access_token )
 print(client.get_athlete())
 print([float(unithelper.miles(i.distance)) for i in client.get_activities()])
 print([i.segment_efforts for i in client.get_activities()])
+
+with open('../Data/JSONData/MainDataset.json') as f:
+    data = json.load(f)
+
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     return(render_template('index.html'))
+
+@app.route("/data")
+def dataset():
+    return(jsonify(data))
 
 if __name__ == '__main__':
     app.run()
---
+++ Analysis/static/app.js
@@ -0,0 +1,7 @@
+d3.json("/data",function(error,data){
+    if(error){
+        console.error()
+    }
+    console.log(data)
+
+});
