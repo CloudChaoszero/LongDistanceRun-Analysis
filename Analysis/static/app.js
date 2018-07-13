1ab6b393731ae7a825a70c9b8f628daaa3eea408
CloudChaoszero <rmaldonadocloud@outlook.com>
2018-07-13 07:36:46 -0700

    Put visual graphing into function. Enables us to use functionality with dropdown value

--- Analysis/static/app.js
+++ Analysis/static/app.js
@@ -1,6 +1,8 @@
+// set svg Width and Height
 var svgWidth = 960;
 var svgHeight = 500;
 
+//set margin
 var margin = {
     top: 20,
     right: 40,
@@ -20,40 +22,89 @@ var chartGroup = svg.append("g")
                 .attr("transform",`translate(${margin.left},${margin.top-2})`);
 
 
+
+//Intializing table
 var table_parent = d3.select(".table-data")
 var d3table = table_parent.append('table')
 
+//Table Header
 thead = d3.select("table").selectAll("th")
 .data(["Activity ID","Distance"]).enter().append("th").text(function(d){return d})
 
 
+//---------------
+
+//Testing Getting info from dropdown
+
+// d3.select("#inds").on("change",function(){
+//     var sect = document.getElementById("inds");
+//     var section = sect.options[sect.selectedIndex].value;
+//     console.log(section);
+
+
+// });
+
+
+
+
+//-------------------------
+
 d3.json("/data",function(error,JSONdata){
+    
     if(error){
         console.error()
     }
+    //console.log(JSONdata);
+    var conditions;
 
+    d3.select("#inds").on("change",function(){
+        var sect = document.getElementById("inds");
+        var section = sect.options[sect.selectedIndex].value;
+        // switch(section){
+        //     case "Mile Coverage Per Activity":
+        //     case "Mile Coverage per Date":
+        //     section = {
+        //         x: data
+        //     }
+        // }
+        console.log(section);    
+        updateGraph(JSONdata,section);
+    });
+
+
+function updateGraph(jsonData, dropdownValue){
+    svg.selectAll("*").remove();
 
     var barSpacing = 10;
     var scaleY = 10;
     var barWidth = 10;
+    //console.log(jsonData);
+    var str0;
+    if(dropdownValue=="Mile Coverage per Activity"){
+        str0="ActivityID"
+    }
+    else{
+        str0="Date"
+    }
 
-    var data = d3.nest().key(function(d){return d.ActivityID;})
+    var data = d3.nest().key(function(d){return d[str0];})
                         .rollup(function(d){
                             return d3.max(d, function(g){return g.Distance * 0.000621371;});
-                        }).entries(JSONdata);
+                        }).entries(jsonData);
+    console.log(data);
    data.forEach(function(d){
-        d.ActivityID = d.key;
+        d[str0] = d.key;
         d.Distance = d.value;
     });
-
     var xBandScale = d3.scaleBand()
-                    .domain(data.map(d => d.ActivityID))
+                    .domain(data.map(d => d[str0]))
                     .range([width,0])
                     .padding(0.1);
     var yLinearScale = d3.scaleLinear()
                     .domain([0,d3.max(data,d=>d.Distance)])
                     .range([height,0]);
     
+
 
     var bottomAxis = d3.axisBottom(xBandScale);
     var leftAxis = d3.axisLeft(yLinearScale);
@@ -65,7 +116,7 @@ d3.json("/data",function(error,JSONdata){
     .attr("y",0)
     .attr("x",-50)
     .attr("transform","rotate(-65)");
-
+    //console.log(data);
     data.forEach(function(d){
         d.Distance = +d.Distance;
 
@@ -87,9 +138,9 @@ d3.json("/data",function(error,JSONdata){
     .classed("bar", true)
     .attr("width", d => xBandScale.bandwidth())
     .attr("height", d => height - yLinearScale(d.Distance))
-    .attr("x", (d, i) => xBandScale(d.ActivityID))
+    .attr("x", (d, i) => xBandScale(d[str0]))
     .attr("y", d => yLinearScale(d.Distance));
-    
+
 
     /* X and Y Axis Labels*/
     chartGroup.append("text")
@@ -99,7 +150,7 @@ d3.json("/data",function(error,JSONdata){
     chartGroup.append("text")
             .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
             .attr("transform", "translate("+ (svgWidth/2) +","+(svgHeight-(100/5)+5)+")")  // centre below axis
-            .text("Activity ID");
+            .text(str0);
 /*Tooltip*/
     var toolTip = d3.tip()
             .attr("class", "tooltip")
@@ -128,7 +179,7 @@ d3.json("/data",function(error,JSONdata){
         .attr("fill","black");
         toolTip.hide(d);
     })
-    
+};
 
 
 });
